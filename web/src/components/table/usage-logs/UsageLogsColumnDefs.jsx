43b2ab2c56817257985cfc50a8d19fcbc8e267b6
xtututu <2516140426@qq.com>
2025-12-25 15:28:44 +0800

    fix: 使用日志中只显示请求模型，不显示重定向后的实际模型

--- web/src/components/table/usage-logs/UsageLogsColumnDefs.jsx
+++ web/src/components/table/usage-logs/UsageLogsColumnDefs.jsx
@@ -23,7 +23,6 @@ import {
   Space,
   Tag,
   Tooltip,
-  Popover,
   Typography,
 } from '@douyinfe/semi-ui';
 import {
@@ -41,7 +40,6 @@ import {
   renderModelPrice,
 } from '../../../helpers';
 import { IconHelpCircle } from '@douyinfe/semi-icons';
-import { Route } from 'lucide-react';
 
 const colors = [
   'amber',
@@ -185,53 +183,12 @@ function renderModelName(record, copyText, t) {
       },
     });
   } else {
-    return (
-      <>
-        <Space vertical align={'start'}>
-          <Popover
-            content={
-              <div style={{ padding: 10 }}>
-                <Space vertical align={'start'}>
-                  <div className='flex items-center'>
-                    <Typography.Text strong style={{ marginRight: 8 }}>
-                      {t('请求并计费模型')}:
-                    </Typography.Text>
-                    {renderModelTag(record.model_name, {
-                      onClick: (event) => {
-                        copyText(event, record.model_name).then((r) => {});
-                      },
-                    })}
-                  </div>
-                  <div className='flex items-center'>
-                    <Typography.Text strong style={{ marginRight: 8 }}>
-                      {t('实际模型')}:
-                    </Typography.Text>
-                    {renderModelTag(other.upstream_model_name, {
-                      onClick: (event) => {
-                        copyText(event, other.upstream_model_name).then(
-                          (r) => {},
-                        );
-                      },
-                    })}
-                  </div>
-                </Space>
-              </div>
-            }
-          >
-            {renderModelTag(record.model_name, {
-              onClick: (event) => {
-                copyText(event, record.model_name).then((r) => {});
-              },
-              suffixIcon: (
-                <Route
-                  style={{ width: '0.9em', height: '0.9em', opacity: 0.75 }}
-                />
-              ),
-            })}
-          </Popover>
-        </Space>
-      </>
-    );
+    // 只显示请求模型，不显示重定向后的实际模型
+    return renderModelTag(record.model_name, {
+      onClick: (event) => {
+        copyText(event, record.model_name).then((r) => {});
+      },
+    });
   }
 }
 
